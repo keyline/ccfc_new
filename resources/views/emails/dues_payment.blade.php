83f16f042b9dcaecc40b5b9f9b644ec4061547e5
deblina1697 <deblina@keylines.net>
2026-01-13 18:08:04 +0530

    modification

--- resources/views/emails/dues_payment.blade.php
+++ resources/views/emails/dues_payment.blade.php
@@ -6,7 +6,7 @@
 <body>
     <h1>Dear Member,</h1>
     <p>Please find your payment link for the dues of {{ $token->memberDue->month_name }} {{ $token->memberDue->year }}.</p>
-    <p><a href="{{ url('/payment/' . $plainTextToken) }}">Pay Now</a></p>
+    <p><a href="{{ url('/member/magic-login/' . $plainTextToken) }}">Pay Now</a></p>
     <p>If you have any questions, please contact us.</p>
     <p>Thank you,</p>
     <p>Your Club</p>

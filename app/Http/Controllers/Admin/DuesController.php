de65f9554e7e1179d1bdfbda2e90533c20aeefa3
deblina1697 <deblina@keylines.net>
2026-01-15 12:22:23 +0530

    payment status modification

--- app/Http/Controllers/Admin/DuesController.php
+++ app/Http/Controllers/Admin/DuesController.php
@@ -186,7 +186,8 @@ class DuesController extends Controller
 
         //$dues = $query->paginate(20);
         $dues = $query->get();
-        print_r($dues); exit;
+        echo "<pre>";
+        print_r($dues->toArray()); exit;
 
 
         return view('admin.dues.list_v2', compact('dues'));

d8bf2c061484a5142bfdcc90f64b6eb0aa1d013e
deblina1697 <deblina@keylines.net>
2026-01-16 17:31:27 +0530

    payment mail template chnged

--- resources/views/emails/dues_payment.blade.php
+++ resources/views/emails/dues_payment.blade.php
@@ -7,7 +7,7 @@
     <h1>Dear Member,</h1>
     <p>Click on the link below to pay your club bill.</p>
     {{-- <p>Please find your payment link for the dues of {{ $token->memberDue->month_name }} {{ $token->memberDue->year }}.</p> --}}
-    <p>Link :<a href="{{ url('/member/magic-login/' . $plainTextToken) }}"></a></p>
+    <p><a href="{{ url('/member/magic-login/' . $plainTextToken) }}">Click Here to Pay</a></p>
     <p>Please note, displayed amount does not include late fee charges, if applicable. Payment less than displayed amount will not be accepted by the system.</p>
     <p>Thank you,</p>
 </body>

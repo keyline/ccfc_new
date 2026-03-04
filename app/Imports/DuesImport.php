4a890491e3d547910c5eba78380d69e6fcdf1a6d
deblina1697 <deblina@keylines.net>
2026-03-05 01:25:21 +0530

    remove debug

--- app/Imports/DuesImport.php
+++ app/Imports/DuesImport.php
@@ -27,7 +27,7 @@ class DuesImport implements ToModel, WithHeadingRow, WithBatchInserts, WithEvent
     */
     public function model(array $row)
     {
-         dd($row);
+        //  dd($row);
         // ++$this->rowCount;
         $this->rowCount++;
         // return new MemberDue([

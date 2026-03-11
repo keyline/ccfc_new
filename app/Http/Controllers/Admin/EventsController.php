ea94dc8a58843899e981dee8c9235fb701f3f16e
subhomoy samanta <subhomoy@keylines.net>
2026-03-11 16:10:30 +0530

    notification bug fixing

--- app/Http/Controllers/Admin/EventsController.php
+++ app/Http/Controllers/Admin/EventsController.php
@@ -250,7 +250,7 @@ class EventsController extends Controller
 
             $type               = 'event';
             $getUserFCMTokens   = UserDevice::select('fcm_token')->where('fcm_token', '!=', '')->groupBy('fcm_token')->get();
-            Helper::pr($getUserFCMTokens);
+            // Helper::pr($getUserFCMTokens);
             // $getUserFCMTokens   = UserDevice::select('fcm_token')->where('fcm_token', '=', 'cBC1nQmrD0uao0lSdZC7dg:APA91bHfY9ATKIiQfjh1X1UIOR__uueNlKlB3P7S7a8qihWxwpbmSXBTh4fgnLz8aYQbgS0pMODeCSuM6h8jt0UZZf0pGASjqvEvax6zAmIlLnFtKnkRaAc')->get();
             $tokens             = [];
             if($getUserFCMTokens){

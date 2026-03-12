8a5d71ace9a2033e72d986a635e439578efa7bd4
Somnath Shil <somnath.shil@keylines.net>
2026-03-12 16:47:27 +0530

    Hdfc payment method issue fixing

--- app/Http/Controllers/Member/PaymentController.php
+++ app/Http/Controllers/Member/PaymentController.php
@@ -612,7 +612,7 @@ class PaymentController extends Controller
             // http_response_code($e->getHttpResponseCode());
             // $response = array("message" => $e->getErrorMessage());
             // error_log($e->getErrorMessage());
-                dd($session);
+                var_dump($session);
                 return response()->json([
                             'error' => true,
                             'message' => $e->getMessage(),

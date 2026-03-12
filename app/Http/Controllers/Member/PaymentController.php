109d245a6d193a2085b5bd911ca80f29b3e248b4
Somnath Shil <somnath.shil@keylines.net>
2026-03-12 11:39:22 +0530

    Hdfc payment method issue fixing

--- app/Http/Controllers/Member/PaymentController.php
+++ app/Http/Controllers/Member/PaymentController.php
@@ -546,8 +546,8 @@ class PaymentController extends Controller
                 $params['order_id'] = $orderId;
                 $params['customer_id'] = 'member_'.$user->id;
                 $params['merchant_id'] = $config["MERCHANT_ID"];
-                $params['customer_email'] = $user->email;
-                $params['customer_phone'] = $user->phone_number_1;
+                $params['customer_email'] = $user->email ?? 'test@test.com';
+                $params['customer_phone'] = $user->phone_number_1 ?? '9999999999';
                 $params['udf1'] = $user->user_code;
                 $params['udf2'] = $user->id;
                 $params['payment_page_client_id'] = $config["PAYMENT_PAGE_CLIENT_ID"];

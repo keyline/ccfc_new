bc0babe0a7b0092611122aa8495142da17e2a32d
Somnath Shil <somnath.shil@keylines.net>
2026-04-07 13:44:19 +0530

    payment gateway fixing

--- app/Http/Controllers/Member/PaymentController.php
+++ app/Http/Controllers/Member/PaymentController.php
@@ -542,19 +542,18 @@ class PaymentController extends Controller
 
                 $params = [];
 
-                // $params['amount'] = $amount;
-                $params['amount'] = "2.0";
+                $params['amount'] = $amount;
                 $params['currency'] = "INR";
-                $params['order_id'] = "4524";
-                // $params['customer_id'] = 'member_'.$user->id;
-                // $params['merchant_id'] = $config["MERCHANT_ID"];
+                $params['order_id'] = $orderId;
+                $params['customer_id'] = 'member_'.$user->id;
+                $params['merchant_id'] = $config["MERCHANT_ID"];
                 $params['customer_email'] = $user->email ?? 'test@test.com';
                 $params['customer_phone'] = $user->phone_number_1 ?? '9999999999';
                 $params['udf1'] = $user->user_code;
                 $params['udf2'] = $user->id;
                 $params['payment_page_client_id'] = $config["PAYMENT_PAGE_CLIENT_ID"];
                 $params['action'] = "paymentPage";
-                // $params['return_url'] = route('member.hdfcsmartpaycallback');
+                $params['return_url'] = route('member.hdfcsmartpaycallback');
 
                 $requestOption = new RequestOptions();
                 $requestOption->withCustomerId('member_'.$user->id);

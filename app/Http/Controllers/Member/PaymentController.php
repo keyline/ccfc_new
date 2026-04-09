6411633a192d062f28cd45150fa8c8aeb9322da2
Somnath Shil <somnath.shil@keylines.net>
2026-04-09 14:17:53 +0530

    juspay new setup uat testing

--- app/Http/Controllers/Member/PaymentController.php
+++ app/Http/Controllers/Member/PaymentController.php
@@ -721,7 +721,7 @@ class PaymentController extends Controller
     {
         try {
             $orderId = uniqid('order_');
-            $result = $juspay->createPaymentSession($orderId, route('payment.success'));
+            $result = $juspay->createPaymentSession($orderId, route('member.hdfcsmartpg'));
 
             return response()->json([
                 'status' => true,

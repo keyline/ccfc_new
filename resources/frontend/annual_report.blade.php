6c4ca94a2a2ac3ae5f044489cf98742616d0aa02
deblina1697 <deblina@keylines.net>
2026-09-04 13:14:42 +0530

    annual report updated

--- resources/frontend/annual_report.blade.php
+++ resources/frontend/annual_report.blade.php
@@ -101,9 +101,9 @@
                                                 <img class="img-fluid" src="{{ asset('img//pdf_downloadicon.png') }}" alt="" />
                                             </div>
                                             <div class="newsletter_right">
-                                                <h3>Annual Report 2024-2025</h3>
+                                                <h3>Annual Report 2025-2026</h3>
                                             </div>
-                                            <a class="wholenewdivlink" href="{{ asset('pdf/CCFC-Annual-Report-2025.pdf') }}" target="_blank"></a>
+                                            <a class="wholenewdivlink" href="{{ asset('pdf/Annual_Report_2025-2026.pdf') }}" target="_blank"></a>
                                         </div>
                                     
                             </div>
